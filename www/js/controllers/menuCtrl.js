7391511c431f733fab61326ae6af55931f8941c2
stolarz88 <stolarz88@comcast.net>
2015-07-02 16:19:33 -0400

    Fixed Menu
    Issues with closing menu on click resolved.

--- www/js/controllers/menuCtrl.js
+++ www/js/controllers/menuCtrl.js
@@ -1,12 +1,16 @@
 angular.module('starter.controllers')
 
-    .controller('MenuCtrl', ['$scope', function ($scope) {
-	  $scope.menu = [
+.controller('MenuCtrl', function($scope, $ionicSideMenuDelegate) {
+  $scope.toggleLeftSideMenu = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+  
+  $scope.menu = [
 		{ title: 'Directory', id: 1, icon: 'briefcase', url:'directory' },
 		{ title: 'Schedule', id: 2, icon: 'calendar', url:'schedule' },
 		{ title: 'Maps', id: 3, icon:'map', url:'maps' },
 		{ title: 'Trivia', id: 4, icon:'ribbon-a' },
 		{ title: 'Presentation', id: 5, icon:'speakerphone' },
-		{ title: 'Home', id: 6, icon:'home' , url:'/'}
+		{ title: 'Home', id: 6, icon:'home' , url:'/app'}
 	  ];
-    }]);
+})
