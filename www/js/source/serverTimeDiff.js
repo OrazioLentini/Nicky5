a43f808c722258ad66f7dbc2089996e4d181b719
Orazio Lentini <oraziio@hotmail.com>
2015-07-02 15:49:07 -0400

    Start of trivia

---
+++ www/js/source/serverTimeDiff.js
@@ -0,0 +1,21 @@
+function serverTimeDifference () {
+	 $.ajax( {
+			url : "http://patty5.com/AppApis/apiServerTime.asp",
+			type: "POST",
+			crossDomain: true,
+			dataType: "jsonp",
+			jsonp: true, 
+			success:function(data){},
+			error: function(jqXHR, textStatus, errorThrown) {}
+		})
+}
+
+function getServerTime (data) {
+	var d = new Date()
+	ClientMilliseconds = d.getTime() - d.setHours(0,0,0,0);
+	ClientTimer = ClientMilliseconds / 1000;
+	iDiff = parseFloat(data.timer) - ClientTimer;
+	localStorage.setItem("ServerClientTimeDiff", iDiff);
+}
+
+serverTimeDifference()
