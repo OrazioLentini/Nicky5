3a810a7046b3abc9c738cc83fab7fea0ce50bf5f
stolarz88 <stolarz88@comcast.net>
2015-07-06 11:30:53 -0400

    Menu Links

--- www/js/controllers/menuCtrl.js
+++ www/js/controllers/menuCtrl.js
@@ -9,8 +9,8 @@ angular.module('starter.controllers')
 		{ title: 'Directory', id: 1, icon: 'briefcase', url:'directory' },
 		{ title: 'Schedule', id: 2, icon: 'calendar', url:'schedule' },
 		{ title: 'Maps', id: 3, icon:'map', url:'maps' },
-		{ title: 'Trivia', id: 4, icon:'ribbon-a' },
+		{ title: 'Trivia', id: 4, icon:'ribbon-a', url:'trivia' },
 		{ title: 'Presentation', id: 5, icon:'speakerphone' },
-		{ title: 'Home', id: 6, icon:'home' , url:'/app'}
+		{ title: 'Home', id: 6, icon:'home' , url:'/app/'}
 	  ];
 })
