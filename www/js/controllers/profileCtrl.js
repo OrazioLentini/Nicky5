9ff548a0a215cac78895ad9f4356afd68f4e8140
stolarz88 <stolarz88@comcast.net>
2015-07-07 14:54:01 -0400

    Commit

--- www/js/controllers/profileCtrl.js
+++ www/js/controllers/profileCtrl.js
@@ -6,12 +6,12 @@ angular.module('starter.controllers')
 
   if(isLoggedIn != null) {
     $scope.data = JSON.parse(isLoggedIn)
-
+    $scope.badgeID = $scope.data[0].BadgeID
+    $scope.userName = $scope.data[0].Username
     $scope.email = $scope.data[0].Email
     $scope.firstName = $scope.data[0].FirstName
     $scope.lastName = $scope.data[0].LastName
-    $scope.badgeID = $scope.data[0].BadgeID
-    $scope.userName = $scope.data[0].Username
+
   }
   $scope.update = function() {
     profileSubmit()
---
+++ www/js/feedback.js
@@ -0,0 +1,27 @@
+//ASK QUESTION----------------------------------------------------------------------------------------------------------------------------------------------------------
+function getFeedback() {
+    alertify.set({ delay: 2000 });
+	alertify.success("Thank you for your feedback")
+	$("#Feedback").val("");
+}
+
+function submitFeedback() {
+	var feedback = $("#Feedback").val();
+
+	var temp = localStorage.getItem('login')
+	data = JSON.parse(temp)
+
+	var username = data[0].Username
+
+ formURL = 'http://patty5.com/AppApis/apiFeedback.asp?Username=' + username + '&Feedback=' + feedback
+   $.ajax( {
+		url : formURL,
+		type: "POST",
+		crossDomain: true,
+		dataType: "jsonp",
+		jsonp: true, 
+		success:function(data){},
+		error: function(jqXHR, textStatus, errorThrown) {}
+	})
+
+};
